8c2eecd74519c2863cda9c2229b17a044e9059df
smartstudioarsitek-sudo <smartstudioarsitek@gmail.com>
2026-02-20 16:00:20 +0700

    Update app_enginex.py

--- app_enginex.py
+++ app_enginex.py
@@ -492,7 +492,7 @@ if selected_menu == "🤖 AI Assistant":
                     3. Format Laporan mengikuti standar Dokumen Lelang (Bab I, II, III...).
                     4. Tampilkan Tabel menggunakan st.table() atau st.dataframe().
                     """
-                    model = genai.GenerativeModel("gemini-robotics-er-1.5-preview", system_instruction=SYS)
+                    model = genai.GenerativeModel("gemini-flash-latest","gemini-robotics-er-1.5-preview", system_instruction=SYS)
                     chat_hist = [{"role": "user" if h['role']=="user" else "model", "parts": [h['content']]} for h in history if h['content'] != prompt]
                     
                     chat = model.start_chat(history=chat_hist)
@@ -745,6 +745,7 @@ elif selected_menu == "🏗️ Audit Struktur":
 
                 except Exception as e:
                     st.error(f"Gagal hitung: {e}")
+
 
 
 
